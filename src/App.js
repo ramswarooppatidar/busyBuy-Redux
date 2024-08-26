27c5ac16630bca0d5f074063ba6e3f4c28936fa3
ramswarooppatidar <ramkatariya1603@gmail.com>
2024-08-26 13:16:03 +0530

    add hash routes for hosting

--- src/App.js
+++ src/App.js
@@ -12,33 +12,34 @@ import { SignIn } from './component/SignIn';
 import { MyOrder } from './component/MyOrder';
 
 function App() {
-  // const router = createHashRouter([
-  //   {
-  //     path: '/',
-  //     element: <Navbar />,
-  //     children: [
-  //       { index: true, element: <Home /> },
-  //       { path: 'signin', element: <SignIn /> },
-  //       { path: 'signup', element: <SignUp /> },
-  //       { path: 'cart', element: <Cart /> },
-  //       { path: 'order', element: <MyOrder /> },
-  //     ],
-  //   },
-  // ]);
+  //for hosting
+  const router = createHashRouter([
+    {
+      path: '/',
+      element: <Navbar />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: 'signin', element: <SignIn /> },
+        { path: 'signup', element: <SignUp /> },
+        { path: 'cart', element: <Cart /> },
+        { path: 'order', element: <MyOrder /> },
+      ],
+    },
+  ]);
 
-  const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Navbar />,
-    children: [
-      {index : true, element : <Home/>},
-      { path: "/sigin", element: <SignIn/> } ,
-      {path : "/sigup", element : <SignUp/>},
-      { path : "/cart", element: <Cart/>},
-      {path : "/order", element : <MyOrder/>} 
-    ]
-  }
-]);
+//   const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Navbar />,
+//     children: [
+//       {index : true, element : <Home/>},
+//       { path: "/sigin", element: <SignIn/> } ,
+//       {path : "/sigup", element : <SignUp/>},
+//       { path : "/cart", element: <Cart/>},
+//       {path : "/order", element : <MyOrder/>} 
+//     ]
+//   }
+// ]);
 
  
   return (
